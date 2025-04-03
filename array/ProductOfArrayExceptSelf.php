d187a4d82668666cf7c38a281a32eed1cae21730
Avinash kant <contact.avinashkantse@gmail.com>
2025-04-03 23:09:39 +0530

    DSA

---
+++ array/ProductOfArrayExceptSelf.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * 238. Product of Array Except Self
+ * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ * You must write an algorithm that runs in O(n) time and without using the division operation.
+ * Example 1:
+ *  Input: nums = [1,2,3,4]
+ *  Output: [24,12,8,6]
+ * Example 2:
+ *  Input: nums = [-1,1,0,-3,3]
+ *  Output: [0,0,9,0,0]
+ */
+
+ class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function productExceptSelf($nums) {
+        $array = [];
+        for($i=0;$i<count($nums);$i++){
+            $product = 1;
+            for($j=0;$j<count($nums);$j++){
+                if($i==$j) continue;
+                $product = $product * $nums[$j];
+            }
+            $array[] = $product;
+        }   
+        return $array;     
+    }
+}
+
+$obj = new Solution;
+print_r($obj->productExceptSelf([1,2,3,4])); //[24,12,8,6]
+print_r($obj->productExceptSelf([-1,1,0,-3,3])); //[0,0,9,0,0]
+?>
---
+++ array/myPow.js
@@ -0,0 +1,17 @@
+
+console.log(Number(42).toString(2));
+/**
+ * 
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+
+ */
+
+
+var myPow = function(x, n) {
+    
+};
+
+console.log(myPow(2,10));
+console.log(myPow(2.1,3));
+console.log(myPow(2,-2));
