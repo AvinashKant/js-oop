94f2eaa3f251ea03daceeee76cd2dacf0217df7f
Avinash kant <contact.avinashkantse@gmail.com>
2025-05-23 15:42:54 +0530

    Array

---
+++ array/painterpartitionproblem.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+var minimum = function (arr , painter){
+
+    console.log(Math.max(arr));
+    console.log(Math.min(arr));
+    return [Math.max(arr), Math.min(arr)]
+}
+
+
+console.log(minimum([40,30,10,20]));
+console.log(minimum([10,10,10,10]));
+console.log(minimum([20,10,40,50]));
