eda69ff38eec6a60a987169408b8a614262e2031
Avinash kant <contact.avinashkantse@gmail.com>
2026-01-18 20:55:31 +0530

    Program

---
+++ python/GenerateMultiplicationTable.py
@@ -0,0 +1,23 @@
+"""
+Docstring for python.GenerateMultiplicationTable
+You are developing a feature in an educational app that displays multiplication tables.
+
+Tasks:
+
+Write a function named multiplication_table that takes a single integer argument number.
+
+Using a for loop and range(), generate the multiplication table for number from 1 to 10.
+
+Return a list of strings in the following format:
+
+"number x i = result"
+
+(Example: "3 x 4 = 12")
+"""
+
+def multiplication_table(number: int) -> list[str]:
+    table = []
+    for i in range(1,10):
+        table.add(f"{number} X {i} = {number*i}")
+    return table
+multiplication_table(3)
